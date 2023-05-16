e665efc0f5058790cc6b95298c77c8c66da5ad85
Jackson Veroneze <jackson@jacksonveroneze.com>
2023-05-16 15:23:27 -0300

    feat: ajustes

--- src/JacksonVeroneze.NET.Extensions/Async/AsyncExtensions.cs
+++ src/JacksonVeroneze.NET.Extensions/Async/AsyncExtensions.cs
@@ -39,4 +39,21 @@ public static class AsyncExtensions
                 .ConfigureAwait(false);
         }
     }
+
+    public static async Task<TType> OnlyOneAsync<TType>(
+        this IEnumerable<Func<CancellationToken, Task<TType>>> funcs)
+    {
+        CancellationTokenSource cancellationTokenSource = new();
+
+        IEnumerable<Task<TType>> tasks = funcs
+            .Select(func => func(cancellationTokenSource.Token));
+
+        Task<TType> first = await Task.WhenAny(tasks)
+            .ConfigureAwait(false);
+        
+        cancellationTokenSource.Cancel();
+
+        return await first
+            .ConfigureAwait(false);
+    }
 }
--- tests/JacksonVeroneze.NET.Extensions.UnitTests/AsyncExtensionsTests.cs
+++ tests/JacksonVeroneze.NET.Extensions.UnitTests/AsyncExtensionsTests.cs
@@ -109,4 +109,47 @@ public class AsyncExtensionsTests
     }
 
     #endregion
+
+    #region OnlyOneAsync
+
+    [Fact(DisplayName = nameof(AsyncExtensions)
+                        + nameof(AsyncExtensions.OnlyOneAsync)
+                        + " : OnlyOneAsync - Success")]
+    public async Task OnlyOneAsync_Success()
+    {
+        // -------------------------------------------------------
+        // Arrange
+        // -------------------------------------------------------
+        IEnumerable<Func<CancellationToken, Task<int>>> tasks = Enumerable.Range(1, 3)
+            .Select(item =>
+            {
+                Func<CancellationToken, Task<int>> func = async ct =>
+                {
+                    int delay = new Random().Next(100, 500);
+
+                    _testOutputHelper.WriteLine($"Item: {item} - Delay: {delay}");
+
+                    await Task.Delay(delay, ct);
+
+                    return item;
+                };
+
+                return func;
+            });
+
+        // -------------------------------------------------------
+        // Act
+        // -------------------------------------------------------
+        int result = await tasks.OnlyOneAsync();
+
+        _testOutputHelper.WriteLine($"Result: {result}");
+
+        // -------------------------------------------------------
+        // Assert
+        // -------------------------------------------------------
+        result.Should()
+            .BeGreaterOrEqualTo(1);
+    }
+
+    #endregion
 }
